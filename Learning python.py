cff28483298b06ad76569103f127cbc7ca6b6476
TT <46784262+TT-World@users.noreply.github.com>
2022-06-08 23:41:03 +0200

    Done for today

--- Learning python.py
+++ Learning python.py
@@ -7,4 +7,34 @@
 #z = -5j
 #print(type(z))
 
+#for x in "matabane":
+ #   print(x, end="")
+ 
+ 
+#txt = "The best things in life are free!"
+#print("free" in txt)
 
+#b = "Hello, world!"
+#print(b[:5])
+
+#b = "Hello, World!"
+#print(b[2:])
+
+#b = "Hello, World!"
+#print(b[-5:-2])
+
+#a = "Hello, World!"
+#print(a.replace("H", "J"))
+
+#age = 36
+#txt = "My name is John, and I am {}"
+#print(txt.format(age))
+
+#quantity = 3
+#itemno = 567
+#price = 49.95
+#yorder = "I want {} pieces of item {} for {} dollars."
+#print(myorder.format(quantity, itemno, price))
+
+x = 200
+print(isinstance(x, int))
